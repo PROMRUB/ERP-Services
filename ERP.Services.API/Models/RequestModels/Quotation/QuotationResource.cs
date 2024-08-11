685876f36c65370a541d60764c0c807bba35d3e6
kornku <kkunayothin@gmail.com>
2024-08-12 01:13:05 +0700

    change quotation date

--- ERP.Services.API/Models/RequestModels/Quotation/QuotationResource.cs
+++ ERP.Services.API/Models/RequestModels/Quotation/QuotationResource.cs
@@ -17,5 +17,5 @@ public class QuotationResource
     public Guid? PaymentAccountId { get; set; }
     public string? QuotationNo { get; set; }
     public int EditTime { get; set; }
-    public string QuotationDateTime { get; set; }
+    public string? QuotationDateTime { get; set; }
 }
