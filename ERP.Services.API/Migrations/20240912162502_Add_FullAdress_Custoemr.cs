d551c5f070e7509b0645ff2e09394bd9752498cf
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2024-09-12 23:27:40 +0700

    [Update] Full Customer Address

---
+++ ERP.Services.API/Migrations/20240912162502_Add_FullAdress_Custoemr.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Services.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_FullAdress_Custoemr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cus_full_address",
+                table: "Customer",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cus_full_address",
+                table: "Customer");
+        }
+    }
+}
