d8a6698bd265ef2c49578b4ce6f42ef25c97fc06
kornku <kkunayothin@gmail.com>
2024-09-26 16:59:46 +0700

    contactlist

--- ERP.Services.API/Services/Customer/CustomerService.cs
+++ ERP.Services.API/Services/Customer/CustomerService.cs
@@ -405,7 +405,7 @@ namespace ERP.Services.API.Services.Customer
             var role = await businessRepository.GetUserBusinessList(userPrincipalHandler.Id, (Guid)organization.OrgId!)
                 .Where(x => x.BusinessId == businessId).FirstOrDefaultAsync();
             List<CustomerContactEntity> result = new List<CustomerContactEntity>();
-            if (!role!.Role!.Contains("SaleManager") && !role!.Role!.Contains("Director"))
+            if (!role!.Role!.Contains("SaleManager") && !role!.Role!.Contains("Director") && !role!.Role!.Contains("Admin"))
             {
                 result = await customerRepository
                     .GetCustomerContactByCustomer((Guid)organization.OrgId, businessId, cusId)
