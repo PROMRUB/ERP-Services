b90b18f95df811bed128ab39ae199005087da4be
Thanakarn Klangkasame <77600906+Simulationable@users.noreply.github.com>
2025-03-26 21:17:11 +0700

    Update QuotationController.cs

--- ERP.Services.API/Controllers/v1/QuotationController.cs
+++ ERP.Services.API/Controllers/v1/QuotationController.cs
@@ -73,6 +73,21 @@ namespace ERP.Services.API.Controllers.v1
             }
         }
 
+        [HttpGet("{quotationId:guid}/product/{productId:guid}")]
+        [MapToApiVersion("1")]
+        public async Task<IActionResult> GetTotalProductQuotation(Guid quotationId, Guid productId)
+        {
+            try
+            {
+                var result = await _service.GetProductPurchaseDetail(quotationId, productId);
+                return Ok(ResponseHandler.Response("1000", null, result));
+            }
+            catch (Exception ex)
+            {
+                return Ok(ResponseHandler.Response(ex.Message, null));
+            }
+        }
+        
         [HttpPost("update_product_quotation")]
         [MapToApiVersion("1")]
         public async Task<IActionResult> UpdateProductQuotation([FromBody] UpdateProductQuotationParameter parammeter)
