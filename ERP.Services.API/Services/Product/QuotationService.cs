147777eb34d6af71452e7aaabe7b9561d6fe533a
Thanakarn Klangkasame <thanakarn.klangkasame@gmail.com>
2025-04-03 23:01:04 +0700

    [Update] Issue.

--- ERP.Services.API/Services/Product/QuotationService.cs
+++ ERP.Services.API/Services/Product/QuotationService.cs
@@ -709,6 +709,7 @@ public class QuotationService : IQuotationService
             throw new Exception("Product not found");
         }
 
+        product.Discount = (float)product.RealPriceMsrp - float.Parse(request.Data.OfferPriceEstimate);
         product.Currency = request.Data.Currency;
         product.Amount = float.TryParse(request.Data.OfferPriceEstimate, NumberStyles.Any, CultureInfo.InvariantCulture, out var offeringPrice) ? offeringPrice : 0f;
         product.LatestCost = decimal.TryParse(request.Data.OfferPriceEstimate, NumberStyles.Any, CultureInfo.InvariantCulture, out var latestCost) ? latestCost : 0m;
